b9ed121d4df04ecffd7d7a66ff7087c7046348ad
marynam99 <SM-PC@DESKTOP-E9O50IE>
2022-04-11 19:59:05 +0900

    Model load

---
+++ app/src/main/java/org/techtown/naro/LoadModel.java
@@ -0,0 +1,108 @@
+package org.techtown.naro;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.media.ThumbnailUtils;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
+import org.techtown.naro.ml.ConvertedModel;
+import org.tensorflow.lite.DataType;
+import org.tensorflow.lite.support.tensorbuffer.TensorBuffer;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+
+
+public class LoadModel extends AppCompatActivity {
+
+    Button camera, gallery;
+    ImageView imageView;
+    TextView result;
+    EditText result2;
+    int imageSize = 299;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_load_model);
+
+
+        camera = findViewById(R.id.button);
+        gallery = findViewById(R.id.button2);
+
+        result = findViewById(R.id.result);
+        imageView = findViewById(R.id.imageView);
+        result2 = findViewById(R.id.result2);
+
+        camera.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.M)
+            @Override
+            public void onClick(View view) {
+                if (checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+                    Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    startActivityForResult(cameraIntent, 3);
+                } else {
+                    requestPermissions(new String[]{Manifest.permission.CAMERA}, 100);
+                }
+            }
+        });
+        gallery.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent cameraIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(cameraIntent, 1);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if(resultCode == RESULT_OK){
+            Bitmap image = null;
+            // handling camera images
+            if(requestCode == 3){
+                image = (Bitmap) data.getExtras().get("data");
+                int dimension = Math.min(image.getWidth(), image.getHeight());
+                image = ThumbnailUtils.extractThumbnail(image, dimension, dimension);
+                imageView.setImageBitmap(image);
+
+                image = Bitmap.createScaledBitmap(image, imageSize, imageSize, false);
+            }
+            // handling gallery images
+            else{
+                Uri dat = data.getData();
+                try {
+                    image = MediaStore.Images.Media.getBitmap(this.getContentResolver(), dat);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                imageView.setImageBitmap(image);
+
+                image = Bitmap.createScaledBitmap(image, imageSize, imageSize, false);
+            }
+            String[] classes = {"folliculitis","impetigo","normal","pyoderma","ringworm"};
+
+            Context context = getApplicationContext();
+            ClassifyImage classifyImage = new ClassifyImage(image, 299, classes, context);
+            result2.setText(classifyImage.result_class);
+
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+}
